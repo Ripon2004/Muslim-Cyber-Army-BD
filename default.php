20bc84a869c3008372337c133791cd0bcbf74425
Me Black_Phish <51786071+Ripon2004@users.noreply.github.com>
2021-01-09 21:03:48 +0600

    Add files via upload

---
+++ default.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html>
+<head> 
+<title>Hacked By Me Black_Phish</title>
+<link rel="SHORTCUT ICON" href="https://cdn1.iconfinder.com/data/icons/ramadhan-kareem-2/512/Ramadhan-22-512.png" type="image/png">
+<meta charset="UTF-8">
+<meta name="description" content="Hello Admin, I Am Not Your Friend. Unfortunately Your Website Hacked By Black_Phish. Don’t Hate Me, Hate The Code, Your Security Is Too Low.">
+<meta name="keywords" content="Black_Phish,Phisher,Spammer,Web Master, Web Developer,Web Security Expert,Hacker,Spammer,Villain,Work,Love Allah,Love Muslim's,Me Not Terrorist,Love Allah,Love Allah,Love Mohammad, My God Allah, Love Allah, Love Mohammad,Love Muslim, Muslim, Love Mohammad, Me Black_Phish, Cyber Space Hacker, Cyber Space, Hacker, Muslim Solider, Esha, Muslim, Allah Hu Akbar, লা ইলাহা ইল্লাল্লাহু মুহাম্মদুর রসুল উল্লাহ">
+<meta name="author" content="BLACK_PHISH">
+<style type="text/css">
+body{ background-color:black; color:#10ef10; width:100%; }
+#phish{ animation: typing 3s; overflow: hidden; white-space: nowrap;}
+@keyframes typing{
+
+  0% {height:0ch; color:#10ef10;}
+  30% { color:#10ef10;}
+  50% { color:red;}
+  70% { color:#10ef10;}
+  100% {height:105ch;}
+
+}
+#txt{
+
+-webkit-text-size-adjust:none; -ms-text-size-adjust:none; -moz-text-size-adjust:none; text-size-adjust:none; }
+</style>
+</head>
+<body>
+<div id="phish">
+<h1>
+<pre style="font-size:30px;width:auto;margin:auto;"> __________
+< Know me! >
+ ----------
+    \
+     \
+                                   .::!!!!!!!:.
+  .!!!!!:.                        .:!!!!!!!!!!!!
+  ~~~~!!!!!!.                 .:!!!!!!!!!UWWW$$$
+      :$$NWX!!:           .:!!!!!!XUWW$$$$$$$$$P
+      $$$$$##WX!:      .^!!!!UW$$$$"  $$$$$$$$#
+      $$$$$  $$$UX   :!!UW$$$$$$$$$   4$$$$$*
+      ^$$$B  $$$$\     $$$$$$$$$$$$   d$$R"
+        "*$bd$$$$      '*$$$$$$$$$$$o+#"
+             """"          """""""
+             </pre><pre style="width:auto;margin:auto; margin-left:50px;" >
+____  _            _      _____  _     _     _     
+ |  _ \| |          | |    |  __ \| |   (_)   | |    
+ | |_) | | __ _  ___| | __ | |__) | |__  _ ___| |__  
+ |  _ <| |/ _` |/ __| |/ / |  ___/| '_ \| / __| '_ \ 
+ | |_) | | (_| | (__|   <  | |    | | | | \__ \ | | |
+ |____/|_|\__,_|\___|_|\_\ |_|    |_| |_|_|___/_| |_|
+                       ______                        
+                      |______|             
+</pre></h1>
+  <br>  
+  <br>                 
+<p id="m" style="width:98%;margin:auto; font-size:40px; color:red;"><span style="font-size:40px; margin-left:5px; border:1px solid green; background-color:green; color:white; padding:10px; border-radius:5px;">root@server</span><b> cat Black_Phish</b></p><br><br></div><p id="txt" style="width:98%;margin:auto; font-size:40px;font-family:ubuntu_monoregular;"></p> 
+<br>
+<center id="cen" style="font-family:ubuntu_monoregular;"></center>         
+<script>
+
+var i = 0;
+
+function typi(){
+
+var x = 0;
+var text = "Hello Admin! It's Me Black_Phish Your Friend. Unfortunately Your Site Has Been Hacked By Me Black_Phish. I Found A Bug In Your System Like Developer Mistake Or Server Software Bug. Please Check Your System And Fix iT. If You Don't Know How To Fix The Bug, Contact With Technician, I Hope He Will Fix The Bug. Thank You. Now About Me I Am Black_Phish From Bangladesh, I am NooB At Web Bug Hunting And Defacing. I Learn Hacking For Make Strong Security. Respect Me, i Will Respect You. And Always Respect One Allah.  ";
+
+if(i < text.length) {
+
+document.getElementById("txt").innerHTML += text.charAt(i);
+i++;
+setTimeout(typi,100);
+}
+}
+function show(){
+ document.getElementById("cen").innerHTML = '<h1 style="font-size:40px;">Greatz:</h1><marquee style="width:80%; margin:auto; font-size:40px;">Black_Phish, Cybertron RA9, HYP0CR!T3 C10WN, 3RROR SCR!P7 and All Black_Phish Friend\'s</marquee><br><br>';
+ 
+ }
+
+setTimeout(typi,4000);
+setTimeout(show,50500);
+</script>
+<img style="display:none;" src="https://scontent.fdac1-1.fna.fbcdn.net/v/t1.0-0/cp0/e15/q65/p240x240/69386135_128549105190860_4083867484443115520_n.jpg?_nc_cat=105&efg=eyJpIjoiYiJ9&_nc_eui2=AeEj8lOQRTA5byX4IvUgK0x3xdPr8Q9wDEmL_85mJE2XC_3fdXusiBxIikw6qZvYrANHs185QJ27hhPBL_--VNdtZlT9vvDm6jW226SVHdEmpPnSSbqbnlEo0CN4ZifGQZw&_nc_oc=AQm32as43yOAPLUuzxtRXB8Sfa4En49a40_Rd90kqM8hFn-qMc_Hu9nFjOTs5zur3tw&_nc_ht=scontent.fdac1-1.fna&oh=3f35bc0d1d1d074d1a5d53657bbd7efc&oe=5DEFF55E">
+</body>
+</html>
